8688f985720086a353347aca9774e0ebd0708f9b
torrijomario@gmail.com <torrijomario@gmail.com>
2026-08-31 11:03:05 +0200

    download video or audio via console

--- yt-dlp-mario.py
+++ yt-dlp-mario.py
@@ -1,6 +1,38 @@
 import yt_dlp
 
-URLS = ['https://www.youtube.com/watch?v=YE7VzlLtp-4']
+def main():
+    end = 0
+    while (end != '1'):
+        url = input("Video or playlist: ")
+        format = input("Audio(1), Video(2): ")
+        if (format == '1'):
+            download_audio(url)
+        elif (format == '2'):
+            download_video(url)
 
-dw = yt_dlp.YoutubeDL()
-dw.download(URLS)
+        end = input("(1) to end: ")    
+
+def download_audio(urls):
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
+    }
+
+    dw = yt_dlp.YoutubeDL(ydl_opts)
+    dw.download(urls)
+
+def download_video(urls):
+    ydl_opts = {
+        'format': 'bv+ba/b',
+        'merge_output_format': 'mp4'
+    }
+
+    dw = yt_dlp.YoutubeDL(ydl_opts)
+    dw.download(urls)
+
+if __name__ == "__main__":
+    main()
